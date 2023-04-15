6d36cf95604779132ea3b3ebcbf1763518383b81
Maurício Caminha <mauricio.caminha@hotmail.com>
2023-04-14 21:14:16 -0300

    Criando comandos básicos para o Study Help

---
+++ commands/comandos.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+const commandsEmbed = new EmbedBuilder()
+	.setColor('Red')
+	.setTitle('Bem vindo ao StudyHelp')
+	.setURL('https://github.com/Mauricio-Caminha/study-help-bot.git')
+	.setDescription('Lista os comandos disponíveis no StudyHelp')
+  .setThumbnail('https://www.uniritter.edu.br/wp-content/uploads/2022/06/imagem_2022-06-01_145619421-removebg-preview.png')
+	.addFields(
+		{ name: '\u200B', value: '\u200B' },
+		{ name: '/comandos', value: 'Lista os comandos disponíveis no StudyHelp'},
+		{ name: '/listar-pre-requisitos', value: 'Lista os pre-requisitos da disciplina escolhida' },
+		{ name: '/otimizar-estudos', value: 'Dá dicas para otimização da rotina de estudos'},
+		{ name: '/plano-de-estudos', value: 'Recomenda conhecimentos básicos para cada disciplina do semestre escolhido'},
+    { name: '\u200B', value: '\u200B' },
+	)
+	.setTimestamp()
+	.setFooter({ text: 'Bons estudos!', iconURL: 'https://www.uniritter.edu.br/wp-content/uploads/2022/06/imagem_2022-06-01_145619421-removebg-preview.png' });
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('comandos')
+    .setDescription('Mostra os comandos executaveis no bot'),
+
+  async execute(interaction) {
+    await interaction.reply({ embeds: [commandsEmbed] });
+  }
+}
---
+++ deploy-commands.js
@@ -0,0 +1,34 @@
+const { REST, Routes } = require('discord.js');
+const dotenv = require('dotenv');
+const fs = require('node:fs');
+const path = require('node:path');
+
+dotenv.config();
+const { TOKEN, APPLICATION_ID, GUILD_ID } = process.env;
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commands = [];
+
+commandFiles.forEach(file => {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+});
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+  try {
+    console.log(`Reseting ${commands.length} commands...`);
+    // console.log(commands);
+
+    const data = await rest.put(
+      Routes.applicationGuildCommands(APPLICATION_ID, GUILD_ID),
+      { body: commands }
+    );
+
+    console.log('Commands registered successfully!')
+  } catch (error) {
+    console.error(error);
+  }
+})();
